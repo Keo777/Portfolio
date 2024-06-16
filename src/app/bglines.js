01862d73b068458a033d2d8bba736cee9e704ee5
Keo777 <keoniis.c@live.com>
2024-06-16 15:26:58 -0700

    stroke animation conditional for ios

--- src/app/bglines.js
+++ src/app/bglines.js
@@ -1,12 +1,22 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
+// Register ScrollTrigger plugin with GSAP
+gsap.registerPlugin(ScrollTrigger);
+
 const BgLines1 = () => {
+    const linesRef = useRef([]);
+
+    useEffect(() => {
+        
+    }, []);
 
     return (
         <div className='lines'>
-        <div className='line'></div>
-        <div className='line'></div>
-        <div className='line'></div>
-        <div className='line'></div>
-        <div className='line'></div>
+            {Array(5).fill().map((_, i) => (
+                <div key={i} ref={el => linesRef.current[i] = el} className='line'></div>
+            ))}
         </div>
     );
 };
