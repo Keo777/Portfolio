66ae4a7dce25ecc50afaed4f3ac895e07fa7ae1d
Keo777 <keoniis.c@live.com>
2024-06-20 18:26:47 -0700

    navbar

--- src/app/page.js
+++ src/app/page.js
@@ -60,47 +60,36 @@ export default function Home() {
   useEffect(() => {
     const animateIntro = async () => {
      
-      
+      const svgPaths = svgRef.current.querySelectorAll('path');
       const tl = gsap.timeline({defaults: {ease: 'power4.inOut'},  });
-      gsap.set(hologramRef.current.avatars[0].current, {
-        x: '60%',
-      });
+      
 
       tl
+        
         .to(hologramRef.current.avatars[0].current, {
-          y: 0,
-          x: 0,
-          duration: 1.5,
-          delay: 0.5,
-        })
-        .to(hologramRef.current.avatars[0].current, {
-          opacity: 1,
-          duration: 2,
-        }, "<")
-        .to(hologramRef.current.avatars[0].current, {
-          x: "-4%",
-          opacity: 0.5,
+          x: "-3%",
+          opacity: 0.9,
           duration: 4,
-        }, "-=.5")
+        }, "+=2.5")
         .to(hologramRef.current.avatars[1].current, {
           x: "0%",
-          opacity: 0.40,
+          opacity: 0.50,
           duration: 4,
         }, "<")
         
         .to(hologramRef.current.avatars[2].current, {
-          x: "4%",
-          opacity: 0.20,
+          x: "3%",
+          opacity: 0.35,
           duration: 4,
         }, "-=4")
         .to(hologramRef.current.avatars[3].current, {
-          x: "8%",
-          opacity: 0.1,
+          x: "6%",
+          opacity: 0.20,
           duration: 4,
           
         }, "-=4")
         .to(hologramRef.current.avatars[4].current, {
-          x: "12%",
+          x: "9%",
           opacity: 0.05,
           duration: 4,
           onComplete: setupScrollAnimations,
@@ -114,7 +103,15 @@ export default function Home() {
           x: 0,
           duration: 1,
           ease: 'power4.inOut',
-        }, "<");
+        }, "<")
+        
+      .to(svgPaths, {
+      y: 0,
+      x: 0,
+      opacity: 1,
+      duration: 1,
+      ease: 'power4.inOut',
+      }, "<");
 
     };
   
@@ -178,20 +175,6 @@ export default function Home() {
         },
       });
 
-      const svgPaths = svgRef.current.querySelectorAll('path');
-    gsap.to(svgPaths, {
-      y: 0,
-      x: 0,
-      opacity: 1,
-      duration: 1,
-      ease: 'power4.inOut',
-      scrollTrigger: {
-        trigger: section1.current,
-        start: 'bottom 50%',
-        toggleActions: 'play none reverse reverse',
-      },
-    });
-
       gsap.to(xlMenu.current, {
         y: "-5rem",
         x: 0,
@@ -253,7 +236,7 @@ export default function Home() {
       const avatar4 = hologramRef.current.avatars[3];
       const avatar5 = hologramRef.current.avatars[4];
       // Mouse movement effect for 3D tilt
-      const maxRotate = 20; // Maximum rotation in degrees
+      const maxRotate = 15; // Maximum rotation in degrees
 
       const handleMouseMove = (e) => {
       const centerX = window.innerWidth / 2;
@@ -262,15 +245,15 @@ export default function Home() {
       const mouseY = e.clientY;
       const deltaX = centerX - mouseX;
       const deltaY = centerY - mouseY;
-      const rotateX = Math.max(-maxRotate, Math.min(maxRotate, deltaY * 0.15)); // Clamp rotationX
-      const rotateY = Math.max(-maxRotate, Math.min(maxRotate, deltaX * 0.15)); // Clamp rotationY
+      const rotateX = Math.max(-maxRotate, Math.min(maxRotate, deltaY * 0.08)); // Clamp rotationX
+      const rotateY = Math.max(-maxRotate, Math.min(maxRotate, deltaX * 0.08)); // Clamp rotationY
 
       animateAvatars(rotateX, rotateY);
       };
 
       const handleDeviceOrientation = (e) => {
-      const rotateX = Math.max(-maxRotate, Math.min(maxRotate, e.beta * 0.15)); // Clamp rotationX from beta
-      const rotateY = Math.max(-maxRotate, Math.min(maxRotate, e.gamma * 0.15)); // Clamp rotationY from gamma
+      const rotateX = Math.max(-maxRotate, Math.min(maxRotate, e.beta * 0.08)); // Clamp rotationX from beta
+      const rotateY = Math.max(-maxRotate, Math.min(maxRotate, e.gamma * 0.08)); // Clamp rotationY from gamma
 
       animateAvatars(rotateX, rotateY);
       };
@@ -279,7 +262,7 @@ export default function Home() {
       const avatars = [avatar1.current, avatar2.current, avatar3.current, avatar4.current, avatar5.current];
       
       avatars.forEach((avatar, index) => {
-      const scale = (index + 0.5) / avatars.length; // Scale factor from 0.2 to 1
+      const scale = (index + 1) / avatars.length; // Scale factor from 0.2 to 1
       const scaledRotateX = rotateX * scale;
       const scaledRotateY = rotateY * scale;
       
@@ -287,9 +270,9 @@ export default function Home() {
         rotationX: scaledRotateX,
         rotationY: -scaledRotateY,
         delay: 0.1 * index,
-        transformPerspective: 800,
+        transformPerspective: 1200,
         ease: "power2.out",
-        duration: 1.5,
+        duration: .5,
       });
 
     });
@@ -308,8 +291,8 @@ export default function Home() {
 
   return (
     <main id="main" className='bg-[#fff] overflow-hidden'>
-      <div ref={nav} className='flex header w-[100%] h-[clamp(40px,5vw,65px)] fixed z-[20] top-[1rem] left-[1rem] justify-between text-[white] font-[monument] mix-blend-difference'>
-        <div className='logo cursor-pointer w-[auto] uppercase'>
+      <div ref={nav} className='flex header w-[100%] min-h-[4rem] px-[5%] h-[clamp(2rem,6vw,5rem)] fixed z-[100] top-[0] left-[0] justify-between items-center border-b-[1px] border-[#ffffff1c] font-[monument] mix-blend-difference'>
+        <div className='logo cursor-pointer w-[auto] uppercase my-[1rem] mix-blend-difference z-[98]'>
         <svg id="herologo" ref={svgRef} width="100%" height="100%" viewBox="0 0 532.16 75.54" preserveAspectRatio="xMinYMin meet" className='cursor-pointer'>
             <path className='translate-y-[-100%] cls-1' stroke="none" strokeWidth="3" strokeLinejoin="round" strokeLinecap="round" fill="rgba(255, 255, 255, 1)"  d="m55.73,34.37l44.76,40.57h-26.28l-32.51-30.35-22.69,16.37v13.97H0V1.94h19v37.86L71.67,1.94h28.82l-44.76,32.43Z"/>
             <path className='translate-y-[-100%] cls-1' stroke="none" strokeWidth="3" strokeLinejoin="round" strokeLinecap="round" fill="rgba(255, 255, 255, 1)"  d="m185.6,37.86C185.6,13.64,199.38.38,231.53.38s45.93,13.36,45.93,37.47-13.88,37.68-45.93,37.68-45.93-13.36-45.93-37.68Zm73.07,0c0-14.82-7.52-21.71-27.14-21.71s-27.14,6.79-27.14,21.71,7.62,21.92,27.14,21.92,27.14-6.99,27.14-21.92Z"/>
@@ -332,10 +315,8 @@ export default function Home() {
             <li className='mx-[clamp(10px,2vw,40px)] my-[2rem]'>Contact</li>
           </ul>
         </div>
-        
-        </div>
-        <div ref={menuContainer} onClick={handleMenuClick} className={`${menuActive ? `${styles.menucontaineractive}` : `${styles.menucontainer}`} xl:translate-x-[150px] fixed top-0 right-0 z-[50] mix-blend-difference`}>
-        <div ref={menuBtn} className='menu absolute top-[clamp(2.25rem,2.25vw,4rem)] right-[clamp(2rem,3vw,5rem)] z-[51]' >
+        <div ref={menuContainer} onClick={handleMenuClick} className={`${menuActive ? `${styles.menucontaineractive}` : `${styles.menucontainer}`} xl:translate-x-[150px] z-[101]`}>
+        <div ref={menuBtn} className='menu top-0 right-0 z-[101]' >
         <label className={`${styles.toggle}`}>
         <input className={`${menuActive && `${styles.active}`} ${styles.toggle}`} type="checkbox" onClick={(e) => e.stopPropagation()} />
         <div>
@@ -359,10 +340,11 @@ export default function Home() {
       </svg>
         </div>
         </div>
-  
-      <nav ref={menu} className='translate-x-[100%] navmenu fixed bg-[#111] border-l-[1px] border-[#ffffff41] h-[100vh] w-full md:w-auto right-0 text-white z-[49]'>
-        <div className='body p-[100px] xl:pr-[200px] 3xl:pr-[300px] flex justify-center md:justify-between'>
-          <div className='nav flex flex-col text-[clamp(2rem,2.5vw,4rem)] font-[monument] uppercase gap-[12px] leading-[clamp(2.5rem,5vw,7rem)] mt-[clamp(1rem,4vh,20rem)]'>
+        
+        </div>
+        <nav ref={menu} className='translate-x-[100%] navmenu fixed bg-[#111] h-[100vh] w-full md:w-auto top-0 right-0 text-white z-[99]'>
+        <div className='body p-[100px] xl:pr-[200px] 3xl:pr-[400px] flex justify-center md:justify-between'>
+          <div className='nav flex flex-col text-[clamp(2rem,2.5vw,3rem)] font-[monument] uppercase gap-[12px] leading-[clamp(2.5rem,5vw,5rem)] mt-[clamp(1rem,4vh,20rem)]'>
             <div className='header'>
               <p className='text-[20px] border-b-[1px] mb-[50px]'>Navigation</p>
               <ul>
@@ -380,12 +362,22 @@ export default function Home() {
       
       
       <div ref={section1} id="section1" className='h-[calc(100vh-60px)] md:h-screen w-full relative bg-[#111]'>
+      <video
+        id="video3"
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="blurred w-[100%] h-[100%] absolute bottom-[0%] left-0 object-cover opacity-21"
+      >
+        <source src="/images/bg30.webm" type="video/webm" />
+      </video>
       <BgLines1 />
       {/*<Image src={'/images/bg-img7.jpg'} width={3072} height={1856} className='w-full h-full absolute top-0 left-0 z-[0]' />*/}
       <div ref={tint} className='hidden md:inline-block bg-[#145363] opacity-0 w-full h-full absolute z-[5] mix-blend-color'></div>
-      {/*<div className='hidden md:inline-block noise w-[400vw] h-[400vh] left-[-100vw] top-[-100vh] mx-auto fixed z-[5]'></div>*/}
+      <div className='hidden lg:inline-block noise w-[400vw] h-[400vh] left-[-100vw] top-[-100vh] mx-auto fixed z-[10]'></div>
        <div className=' h-full w-full left-0 right-0 mx-auto absolute overflow-hidden'>
-       <HologramImg ref={hologramRef} numImages={5} src={'/images/keoniis-55.webp'} translateX={'0%'} opacity={0} width={1400} height={1473} />
+       <HologramImg ref={hologramRef} numImages={5} src={'/images/keoniis-62.webp'} translateX={'0%'} opacity={0} width={1400} height={1473} />
       
         <DynamicHero5 activeVideo={activeVideo} toggleVideo={toggleVideo} cycleVideo={cycleVideo} />
         
